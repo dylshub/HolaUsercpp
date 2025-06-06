2f420fb36b2664cb0baf33a342202a382f1d0c6b
JUANMAWASD <147288828+JUANMAWASD@users.noreply.github.com>
2025-06-05 20:55:21 -0600

    juan miculax

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 using namespace std;
+int edad;
 
 int main(){
     string user;
@@ -9,10 +10,15 @@ int main(){
 
     cin>>user;
 
+    cout<<"Ingrese su edad "<<endl;
+
+    cin>>edad;
+    
     if (user != "" || user != " "){
         cout<<"Hola "<<user<<endl;
     }else{
         cout <<"HOLA MUNDO"<<endl;
+        cout<<"Tu tienes "<<edad<<" años";
     }
 
 }
