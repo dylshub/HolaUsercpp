f6d922baf00caabf3f84cc5414ca6a2e46e0a7bc
gudymtm <gudygithutchamale@gmail.com>
2025-06-05 23:05:14 -0600

    Update Main.cpp
    Mejore el hola users .cpp

--- Main.cpp
+++ Main.cpp
@@ -1,18 +1,58 @@
+
 #include <iostream>
 
 using namespace std;
 
 int main(){
-    string user;
+    string nombre;
+    int opcion;
     
-    cout<<"Ingrese su nombre "<<endl;
-
-    cin>>user;
-
-    if (user != "" || user != " "){
-        cout<<"Hola "<<user<<endl;
-    }else{
-        cout <<"HOLA MUNDO"<<endl;
+    cout << "Hola! Ingresa tu nombre: ";
+    getline(cin, nombre);
+    
+    
+    if (nombre == "" || nombre == " "){
+        cout << "HOLA MUNDO" << endl;
+        return 0;
     }
-
+    
+    cout << "\nHola " << nombre << "!" << endl;
+    
+    
+    cout << "\nQue quieres hacer?" << endl;
+    cout << "1 - Ver cuantas letras tiene mi nombre" << endl;
+    cout << "2 - Ver mi nombre al reves" << endl;
+    cout << "3 - Salir" << endl;
+    cout << "Elige: ";
+    cin >> opcion;
+    
+    if(opcion == 1){
+        cout << "\nTu nombre tiene " << nombre.length() << " letras" << endl;
+        
+        
+        int vocales = 0;
+        for(int i = 0; i < nombre.length(); i++){
+            char letra = nombre[i];
+            if(letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u' ||
+               letra == 'A' || letra == 'E' || letra == 'I' || letra == 'O' || letra == 'U'){
+                vocales++;
+            }
+        }
+        cout << "Y tiene " << vocales << " vocales" << endl;
+    }
+    else if(opcion == 2){
+        cout << "\nTu nombre al reves: ";
+        for(int i = nombre.length()-1; i >= 0; i--){
+            cout << nombre[i];
+        }
+        cout << endl;
+    }
+    else if(opcion == 3){
+        cout << "\nAdiós " << nombre << "!" << endl;
+    }
+    else{
+        cout << "\nOpcion incorrecta" << endl;
+    }
+    
+    return 0;
 }
